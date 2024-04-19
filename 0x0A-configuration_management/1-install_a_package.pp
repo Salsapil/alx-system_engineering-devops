633e1943d7d183c7731dcd262b6e09fba16fda15
Salsabil <salsabeelahmed323@gmail.com>
2024-04-20 01:43:59 +0200

    task1 edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,4 @@
 # Using Puppet, install flask from pip3.
-class { 'python::pip': }
-
-package { 'Flask':
-  ensure => installed,
-  provider => 'pip3',
-  require => Class['python::pip'],
-  version => '2.1.0',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
