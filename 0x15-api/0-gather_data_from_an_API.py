53df7657456ab771ce14f54f1321e82d95ff5ce5
Salsabil <salsabeelahmed323@gmail.com>
2024-05-20 16:30:49 +0300

    0x15-api_Task0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+
+    employee_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(employee_id)).json()
+
+    params = {"userId": employee_id}
+    todos = requests.get(url + "todos", params).json()
+
+    completed = [i.get("title") for i in todos if i.get("completed") is True]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+
+    for complete in completed:
+        print("     {}".format(complete))
