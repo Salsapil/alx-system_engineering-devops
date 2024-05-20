1d8b2afa46a62b4a971b2a36e5ebfa6ae59048f6
Salsabil <salsabeelahmed323@gmail.com>
2024-05-20 17:03:58 +0300

    0x15-api_Task2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Exports to-do list information for a given employee ID to JSON format.
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+
+    url = "https://jsonplaceholder.typicode.com/"
+
+    user = requests.get(url + "users/{}".format(user_id)).json()
+
+    username = user.get("username")
+    params = {"userId": user_id}
+    todos = requests.get(url + "todos", params).json()
+
+    data_to_export = {
+        user_id: [
+            {
+                "task": i.get("title"),
+                "completed": i.get("completed"),
+                "username": username
+            }
+            for i in todos
+        ]
+    }
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump(data_to_export, jsonfile, indent=4)
